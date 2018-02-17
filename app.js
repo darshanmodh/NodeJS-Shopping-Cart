580a7bed791e54be9e2ef87bcc1afc3b23c3b105
Darshan Modh <darshanmodh@gmail.com>
2018-02-17 13:39:09 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,72 @@
+var express = require('express');
+var path = require('path');
+var mongoose = require('mongoose');
+var config = require('./config/database');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var expressValidator = require('express-validator');
+
+mongoose.connect(config.database);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
+db.on('open', function() {
+    console.log('connected to MongoDB');
+});
+
+var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.locals.errors = null;
+
+// body parser
+app.use(bodyParser.urlencoded( {extended: false} ));
+app.use(bodyParser.json());
+
+// express-sessions
+app.use(session({
+    secret: 'keyboard cat',
+    resave: true,
+    saveUninitialized: true
+    // cookie: { source: true }
+}));
+
+// express-validator
+app.use(expressValidator({
+    errorFormatter: function(param, msg, value) {
+        var namespace = param.split('.'),
+            root = namespace.shift(),
+            formParam = root;
+
+        while(namespace.length) {
+            formParam += '[' + namespace.shift() + ']';
+        }
+
+        return {
+            param: formParam,
+            msg: msg,
+            value: value
+        };        
+    }
+}));
+
+// express-messages
+app.use(require('connect-flash')());
+app.use(function(req, res, next) {
+    res.locals.messages = require('express-messages') (req, res);
+    next();
+});
+
+var pages = require('./routes/pages.js');
+var adminPages = require('./routes/adminPages.js');
+
+app.use('/', pages);
+app.use('/admin/pages', adminPages);
+
+var port = 3000;
+app.listen(port, function() {
+    console.log('Server started on port ' + port);
+});
---
+++ models/page.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+
+var pageSchema = mongoose.Schema( {
+    title: {
+        type: String,
+        required: true
+    },
+    slug: {
+        type: String
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    sorting: {
+        type: Number
+    }
+});
+
+var Page = module.exports = mongoose.model('pages', pageSchema);
