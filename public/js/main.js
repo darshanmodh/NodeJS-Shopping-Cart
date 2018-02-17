d7da1355c4cb56ccd9d9f6f41a6a362c32c42e5e
Darshan Modh <darshanmodh@gmail.com>
2018-02-17 13:58:16 -0800

    Page deletion

--- public/js/main.js
+++ public/js/main.js
@@ -2,4 +2,9 @@ $(function() {
     if($('textarea#ta').length) {
         CKEDITOR.replace('ta');
     }
+
+    $('a.confirmDeletion').on('click', function() {
+        if(!confirm('Confirm Deletion')) 
+            return false;
+    });
 });
